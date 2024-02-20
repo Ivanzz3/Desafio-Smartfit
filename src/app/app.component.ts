fbf4e16f5a1626b43e9964e430bad49e9366146a
Ivan <junior.pereira09@aluno.ifce.edu.br>
2024-02-20 10:45:13 -0300

    chore: add aditional syling and image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {GetUnitsService} from "./services/get-units.service";
+import { Location} from "./types/location.interface";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'desafio-smartfit';
+  showList = new BehaviorSubject(false);
+  unitsList : Location [] = [];
+  constructor(private unitService: GetUnitsService) {
+
+  }
+  onSubmit() {
+   this.showList.next(true);
+   this.unitsList = this.unitService.getFilteredUnits( );
+  }
 }
